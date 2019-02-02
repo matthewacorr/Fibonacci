7ab3318cf861529986cc5508a9b8f052e39b1c2b
tothecorr <thematthewcorr@gmail.com>
2019-02-02 16:39:55 -0500

    Added first implementation of recursive method, known issue that the recursive method prints an extra iteration

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,24 +2,39 @@ public class Fibonacci {
 
     public static long fiboSeriesRec(int elements){
         long startRecTime = System.nanoTime(); // Start timer for recursive method
-        // Recursive loop goes here
+
+        System.out.print("0 "); // Hardcoded 0
+        for(int i = 1; i <= elements; i++){
+            System.out.print(fiboRecLoop(i) +" ");
+        }
+
         long endRecTime = System.nanoTime(); // End timer for recursive method
         long recTime = endRecTime - startRecTime; // Calculate total time
         return recTime;
+    }
+
+    public static long fiboRecLoop(int elements){
+        if(elements == 1 || elements == 2){
+            return 1;
+        }
+
+        return fiboRecLoop(elements - 1) + fiboRecLoop(elements - 2);
     }
 
     public static long fiboSeriesIte(int elements){
         long n1=0, n2=1, n3;
 
         long startIteTime = System.nanoTime(); // Start timer for iterative method
-        System.out.print(n1 + " ");
-        System.out.print(n2 + " ");
+        System.out.print(n1 + " "); // Print initial n1 value
+        System.out.print(n2 + " "); // Print initial n2 value
+
         for(int i=2;i<elements;i++){
             n3 = n1 + n2;
-            System.out.print(n3 + " ");
-            n1 = n2;
-            n2 = n3;
+            System.out.print(n3 + " "); // Print sum of last two numbers
+            n1 = n2; // Shift n1 value
+            n2 = n3; // Shift n2 value
         }
+
         long endIteTime = System.nanoTime(); // End timer for iterative method
         long iteTime = endIteTime - startIteTime; // Calculate total time
         return iteTime;
