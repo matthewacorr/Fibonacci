cd93f3936b9a4bf9e20cc44c03ec48019ebe28b3
tothecorr <thematthewcorr@gmail.com>
2019-02-02 11:37:53 -0500

    Updated README and added program description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fibonacci
+
+- This program compares the speed of two different methods which generate numbers in the fibonacci sequence
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+/*
+    Name: Matthew Corr (0626013)
+    Date: Feb 3rd 2019
+
+    This program was made for COIS2240, it compares two the speeds of different iterative/recursive methods
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        
     }
 }
