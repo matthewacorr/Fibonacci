763fa1e9f2eaeb9d9c34f719f5d90826b7718727
tothecorr <thematthewcorr@gmail.com>
2019-02-02 17:24:55 -0500

    Added milliseconds display and formatted output of driver to make it more appealing

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,21 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         // Welcome the user
-        System.out.println("Welcome to the Fibonacci tester!");
-        System.out.println("Please enter the number of elements to test");
-        System.out.println("--------------------------------------------");
+        System.out.println(" _______ __ __                                 __ ");
+        System.out.println("|    ___|__|  |__._____._____.___._.____.____.|__|");
+        System.out.println("|    ___|  |  _  |  _  |     |  _  |  __|  __||  |");
+        System.out.println("|___|   |__|_____|_____|__|__|___._|____|____||__|");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("|            Please enter the amount of iterations to perform            |");
+        System.out.println("-------------------------------------------------------------------------");
 
         // Take in the amount of elements to print
         System.out.print("> ");
         int elements = in.nextInt();
 
-        System.out.println("\n--------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------");
         System.out.println("Calculating for " + elements + " iterations.");
-        System.out.println("--------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------");
         // Testing methods
         System.out.print("Testing iterative method: ");
         long iteTime = Fibonacci.fiboSeriesIte(elements);
@@ -33,9 +37,12 @@ public class Main {
         long recTime = Fibonacci.fiboSeriesRec(elements);
 
         // Print out time stamps
-        System.out.println("\n--------------------------------------------------------------------");
-        System.out.println("Time taken for iterative method: " + iteTime + " nanoseconds");
-        System.out.println("Time taken for recursive method: " + recTime + " nanoseconds");
-        System.out.println("--------------------------------------------------------------------");
+        System.out.println("\n------------------------------------------------------------------------");
+        System.out.println("Time taken for iterative method: " + iteTime + " nanoseconds / " + (iteTime/1000000) + " milliseconds");
+        System.out.println("Time taken for recursive method: " + recTime + " nanoseconds / " + (recTime/1000000) + " milliseconds");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Thanks for playing, goodbye!");
+        System.out.println("------------------------------------------------------------------------");
+        System.exit(0);
     }
 }
