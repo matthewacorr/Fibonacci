5bd1d28516c4461fb82aa8c46f256f8186f8df00
tothecorr <thematthewcorr@gmail.com>
2019-02-02 12:13:13 -0500

    Added method calls and print statements to main

---
+++ src/Fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci {
+
+    public static long fiboSeriesRec(int elements){
+        
+    }
+
+    public static long fiboSeriesIte(int elements){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,27 @@
 
     This program was made for COIS2240, it compares two the speeds of different iterative/recursive methods
  */
+import java.util.Scanner; // Import Scanner to read lines
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        // Create the scanner
+        Scanner in = new Scanner(System.in);
+
+        // Welcome the user
+        System.out.println("Welcome to the Fibonacci tester!");
+        System.out.println("Please enter the number of elements to test");
+        System.out.println("--------------------------------------------");
+
+        // Take in the amount of elements to print
+        System.out.print("> ");
+        int elements = in.nextInt();
+
+        System.out.println("Testing iterative method:");
+        Fibonacci.fiboSeriesIte(elements);
+        System.out.println("Testing recursive method:");
+        Fibonacci.fiboSeriesRec(elements);
     }
 }
